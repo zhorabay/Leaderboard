f9b49e82f1e7a21b627a67410ca68ffc0375c229
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-08-29 11:47:38 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './style.css';
-import Player from './modules/player';
-import add from './modules/add.js';
+import Player from './modules/player.js';
+import Add from './modules/add.js';
 
 document.getElementById('add').addEventListener('submit', (e) => {
-    const name = document.getElementById('name').value;
-    const score = document.getElementById('score').value;
-  
-    const player = new Player(name, score);
-    const addScore = new add();
-  
-    if (name === '' || score === '') {
-      addScore.showAlert('Please fill in all fields', 'error');
-    } else {
-      addScore.addPlayerToList(player);
-      addScore.showAlert('Player Added', 'success');
-      addScore.clearFields();
-    }
-    e.preventDefault();
-  });
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+
+  const player = new Player(name, score);
+  const addScore = new Add();
+
+  if (name === '' || score === '') {
+    addScore.showAlert('Please fill in all fields', 'error');
+  } else {
+    addScore.addPlayerToList(player);
+    addScore.showAlert('Player Added', 'success');
+    addScore.clearFields();
+  }
+  e.preventDefault();
+});
